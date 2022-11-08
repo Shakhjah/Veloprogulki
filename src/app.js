1882e17ff282d4f2f517e212d974f08123839f34
Shakhjah <shakhprodukti@mail.ru>
2022-11-08 11:22:03 +0300

    lolo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 require('dotenv').config(); // реквайрим .env
 
 // Внешние импорты
-const path = require('path'); // подключаем css
 const express = require('express');
-const ws = require('ws')
+const app = express();
+const path = require('path'); // подключаем css
 const morgan = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session); // хранилище для сессий
@@ -15,7 +15,7 @@ const renderTemplate = require('../src/lib/RenderTemplate');
 const { SESSION_SECRET } = process.env;
 const PORT = process.env.PORT || 3000;
 
-const httpServer = app.listen(PORT, async () => {
+    app.listen(PORT, async () => {
     try {
       await sequelize.authenticate();
       console.log('Соединение с базой установлено!');

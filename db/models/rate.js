958d0ba537b8e5beb95d89a16104997956721985
Igor.K <igor.k.nsk@gmail.com>
2022-11-08 18:24:47 +0700

    reDb

--- db/models/rate.js
+++ db/models/rate.js
@@ -9,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({User, Biketrack}){
+    static associate({ User, BikeTrack }) {
       this.hasMany(User, { foreignKey: 'userId' }),
-      this.hasMany(Biketrack, { foreignKey: 'trackId' })
+        this.hasMany(BikeTrack, { foreignKey: 'trackId' })
     }
   }
   Rate.init({
--- db/models/user.js
+++ db/models/user.js
@@ -9,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Rate, Biketrack, Comment }) {
+    static associate({ Rate, BikeTrack, Comment }) {
       this.hasMany(Rate, {foreignKey: 'userId'} ),
-      this.hasMany(Biketrack, {foreignKey: 'userId'} ),
+        this.hasMany(BikeTrack, { foreignKey: 'userId' }),
       this.hasMany(Comment, {foreignKey: 'userId'} )
     }
   }

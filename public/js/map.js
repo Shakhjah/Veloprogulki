c92490173fa2b6eea4c7fe05db35304e6f0090b7
Igor.K <igor.k.nsk@gmail.com>
2022-11-10 02:30:53 +0700

    map from and to undefined

--- public/js/map.js
+++ public/js/map.js
@@ -7,10 +7,47 @@ ymaps.ready(() => {
   }, {
     searchControlProvider: 'yandex#search',
   });
-
+  // Получение ссылки на панель.
   const control = myMap.controls.get('routeButtonControl');
   control.routePanel.state.set({
     type: 'bicycle',
+  });
+  // Получение мультимаршрута.
+  const multiRoutePromise = control.routePanel.getRouteAsync();
+  multiRoutePromise.then((multiRoute) => {
+    // Подписка на событие обновления мультимаршрута.
+    multiRoute.model.events.add('requestsuccess', () => {
+      // Получение ссылки на активный маршрут.
+      const activeRoute = multiRoute.getActiveRoute();
+      // Когда панель добавляется на карту, она
+      // создает маршрут с изначально пустой геометрией.
+      // Только когда пользователь выберет начальную и конечную точки,
+      // маршрут будет перестроен с непустой геометрией.
+      // Поэтому для избежания ошибки нужно добавить проверку,
+      // что маршрут не пустой.
+      if (activeRoute) {
+        multiRoute.options.set({
+          // ----- Настройка вида
+          // Цвет метки начальной точки.
+          wayPointStartIconFillColor: 'red',
+          // Цвет метки конечной точки.
+          wayPointFinishIconFillColor: 'blue',
+          // Внешний вид линии активного маршрута.
+          routeActiveStrokeWidth: 8,
+          routeActiveStrokeStyle: 'solid',
+          routeActiveStrokeColor: '#002233',
+          // Внешний вид линий альтернативных маршрутов.
+          routeStrokeStyle: 'dot',
+          routeStrokeWidth: 3,
+          boundsAutoApply: true,
+        });
+        // Вывод информации об активном маршруте.
+        console.log(`Длина: ${activeRoute.properties.get('distance').text}`);
+        console.log(`Время прохождения: ${activeRoute.properties.get('duration').text}`);
+      }
+    });
+  }, (err) => {
+    console.log(err);
   });
 
   control.routePanel.options.set({
@@ -22,87 +59,69 @@ ymaps.ready(() => {
     routeStrokeColor: '00FF00',
   });
 
-  // Создаем кнопку, с помощью которой пользователи смогут менять местами начальную и конечную точки маршрута.
+  // Создаем кнопку, с помощью которой пользователи смогут
+  //  менять местами начальную и конечную точки маршрута.
   const switchPointsButton = new ymaps.control.Button({
-    data: { content: 'Поменять местами', title: 'Поменять точки местами' },
+    data: { content: 'Поменять местами точки А и В', title: 'Поменять точки местами' },
     options: { selectOnClick: false, maxWidth: 160 },
   });
-  // // Объявляем обработчик для кнопки.
-  // switchPointsButton.events.add('click', (event) => {
-  //   // Меняет местами начальную и конечную точки маршрута.
-  //   control.routePanel.switchPoints();
-  // });
-  // myMap.controls.add(switchPointsButton);
+  // Объявляем обработчик для кнопки.
+  switchPointsButton.events.add('click', (event) => {
+    // Меняет местами начальную и конечную точки маршрута.
+    control.routePanel.switchPoints();
+  });
+  myMap.controls.add(switchPointsButton);
 
   // my button Сохранить маршрут
   const mapSaveButton = document.getElementById('mapSaveId');
-  ymaps.domEvent.manager.add(mapSaveButton, 'click', (event) => {
+  ymaps.domEvent.manager.add(mapSaveButton, 'click', async (event) => {
     // event.preventDefault();
-    const activeRoute = control.routePanel.state.get({});
+    let activeRoute = control.routePanel.state.get({});
     console.log('▶ ⇛ activeRoute', activeRoute);
     console.log('▶ ⇛ FROM', activeRoute.from);
     console.log('▶ ⇛ TO', activeRoute.to);
     console.log('▶ ⇛ TYPE', activeRoute.type);
+    // Создаем маршрут из полученных координат для получения данных
+    const multiRoute = new ymaps.multiRouter.MultiRoute({
+      referencePoints: [
+        activeRoute.from,
+        activeRoute.to,
+      ],
+      params: {
+        // Тип маршрутизации - пешеходная маршрутизация.
+        routingMode: 'bicycle',
+      },
+    }, {
+      boundsAutoApply: true,
+    });
+    //-------------------------
+    //-------------------------
+    // Подписка на событие обновления данных маршрута.
+    multiRoute.model.events.add('requestsuccess', () => {
+      // Получение ссылки на активный маршрут.
+      activeRoute = multiRoute.getActiveRoute();
+      // Вывод информации о маршруте.
+      console.log(`Длина3: ${activeRoute.properties.get('distance').text}`);
+      console.log(`Время прохождения3: ${activeRoute.properties.get('duration').text}`);
+      console.log('▶ ⇛ FROM333', activeRoute.from);
+      console.log('▶ ⇛ TO333', activeRoute.to);
+      // -------------------- Формируем обьект для отправки на сервер с картой
+
+      // Для автомобильных маршрутов можно вывести
+      // информацию о перекрытых участках.
+      if (activeRoute.properties.get('blocked')) {
+        console.log('На маршруте имеются участки с перекрытыми дорогами.');
+      }
+    });
   });
 });
 
-// ------------------- 2 MAP
-// ymaps.ready(() => {
-//   const myMap = new ymaps.Map('map2', {
-//     center: 'auto',
-//     zoom: 12,
-//     controls: ['zoomControl'], // Элементы управления
-//     behaviors: ['drag', 'multiTouch', 'scrollZoom'],
-//   }, {
-//     searchControlProvider: 'yandex#search',
-//   });
-
-//   // const control = myMap.controls.get('routeButtonControl');
-//   // control.routePanel.state.set({
-//   //   type: 'bicycle',
-//   // });
-
-//   // control.routePanel.options.set({
-//   //   types: {
-//   //     bicycle: true,
-//   //     pedestrian: true,
-//   //     masstransit: false,
-//   //   },
-//   //   routeStrokeColor: '00FF00',
-//   // });
-
-//   const multiRoute = new ymaps.multiRouter.MultiRoute({
-//     referencePoints: [
-//       [55.85810611088341, 38.440435433879074],
-//       [55.88341498860973, 38.45050623291407],
-//     ],
-//     params: {
-//       // Тип маршрутизации
-//       routingMode: 'bicycle',
-//     },
-//   }, {
-//     // Опция editorDrawOver запрещает ставить точки поверх объектов карты
-//     // (в режиме добавления новых точек). Это нужно для того,
-//     // чтобы пользователи могли создавать промежуточные
-//     // точки по линии маршрута.
-//     editorDrawOver: true,
-//     // Опция editorMidPointsType задает тип промежуточных точек,
-//     // которые будут создаваться на маршруте.
-//     // "via" - будут создаваться транзитные точки;
-//     // "way" - путевые точки.
-//     editorMidPointsType: 'via',
-//   });
-//   // Добавление маршрута на карту.
-//   myMap.geoObjects.add(multiRoute);
-// });
-
+// ----------------------------------- 2 карта
 function init() {
   // Задаём точки мультимаршрута.
   const pointA = [55.85810611088341, 38.440435433879074];
   const pointB = [55.88341498860973, 38.45050623291407];
-  /**
-       * Создаем мультимаршрут.
-       */
+  // Создаем мультимаршрут.
   const multiRoute = new ymaps.multiRouter.MultiRoute({
     referencePoints: [
       pointA,
@@ -116,8 +135,27 @@ function init() {
     // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
     boundsAutoApply: true,
   });
-
-  // Создаем кнопку.
+  // ----- Настройка вида
+  multiRoute.options.set({
+    // Цвет метки начальной точки.
+    wayPointStartIconFillColor: 'red',
+    // Цвет метки конечной точки.
+    wayPointFinishIconFillColor: 'blue',
+    // Внешний вид линии активного маршрута.
+    routeActiveStrokeWidth: 8,
+    routeActiveStrokeStyle: 'solid',
+    routeActiveStrokeColor: '#002233',
+    // Внешний вид линий альтернативных маршрутов.
+    routeStrokeStyle: 'dot',
+    routeStrokeWidth: 3,
+    boundsAutoApply: true,
+  });
+  // Включение режима редактирования.
+  // multiRoute.editor.start();
+  // А вот так можно отключить режим редактирования.
+  // multiRoute.editor.stop();
+  //---------------------------
+  // Создаем кнопку. Поменять местами.
   const changePointsButton = new ymaps.control.Button({
     data: { content: 'Поменять местами точки А и В' },
     options: { selectOnClick: true },
@@ -140,67 +178,27 @@ function init() {
   }, {
     buttonMaxWidth: 300,
   });
-  const geo = ymaps.geocode('Moscow');
-  geo.then(
-    (res) => {
-      // Выведем в консоль данные, полученные в результате геокодирования объекта.
-      console.log('GEO', res.geoObjects.get(0).properties.get('metaDataProperty').getAll());
-    },
-    (err) => {
-      // Обработка ошибки.
-    },
-  );
-  geo.then((res) => console.log('RES', res));
+
   // Получение инфо о маршруте
   multiRoute.model.events.add('requestsuccess', () => {
     // Получение ссылки на активный маршрут.
     const activeRoute = multiRoute.getActiveRoute();
-    console.log('▶ ⇛ activeRoute', activeRoute.getCountry());
-
-    // console.log('▶ ⇛ geo', geo);
     // Вывод информации о маршруте.
-    console.log(`Длина: ${activeRoute.properties.get('distance').text}`);
-    console.log(`Время прохождения: ${activeRoute.properties.get('duration').text}`);
+    console.log(`distance-MAP-2: ${activeRoute.properties.get('distance').text}`);
+    console.log(`Time-MAP-2: ${activeRoute.properties.get('duration').text}`);
+    // console.log(activeRoute);
+
     // Для автомобильных маршрутов можно вывести
     // информацию о перекрытых участках.
-    console.log('На маршруте имеются участки с перекрытыми дорогами.');
+    if (activeRoute.properties.get('blocked')) {
+      console.log('На маршруте имеются участки с перекрытыми дорогами.');
+    }
   });
 
-  // const myGeocoder = ymaps.geocode('Moscow');
-  // myGeocoder.then(
-  //   (res) => {
-  //     // map.geoObjects.add(res.geoObjects);
-  //     // Выведем в консоль данные, полученные в результате геокодирования объекта.
-  //     console.log('GEO', res.geoObjects.get(0).getLocalities('Moscow'));
-  //     // console.log(res.geoObjects.get(0).getLocalities());
-  //   },
-  // )
-  //   .catch((err) => console.log);
-  // myMap.getAdministrativeAreas('Moscow');
-
-  // const geoCoder = ymaps.geocode('Moscow');
-  // geoCoder.then((result) => {
-  //   console.log(result.geoObjects.get(0).getLocalities());
-  // });
-  // Получение инфо о маршруте
-  //   multiRoute.model.properties('requestsuccess', () => {
-  //     // Получение ссылки на активный маршрут.
-  //     const activeRoute = multiRoute.getActiveRoute();
-  //     // Вывод информации о маршруте.
-  //     console.log(`activeRoute: ${activeRoute.propertiesЪ}`);
-  //     console.log(activeRoute);
-  //     // Для автомобильных маршрутов можно вывести
-  //     // информацию о перекрытых участках.
-  //     if (activeRoute.properties.get('blocked')) {
-  //       console.log('На маршруте имеются участки с перекрытыми дорогами.');
-  //     }
-  //   });
-
-  //   // Добавляем мультимаршрут на карту.
-  //   myMap.geoObjects.add(multiRoute);
+  // Добавляем мультимаршрут на карту.
+  myMap.geoObjects.add(multiRoute);
 }
 
 ymaps.ready(init);
 
-// https://geocode-maps.yandex.ru/1.x/?apikey=ee11c971-3558-49d6-8fae-209f13ccaf25&geocode=[55.85810611088341, 38.440435433879074]
-// https://geocode-maps.yandex.ru/1.x/?apikey=ee11c971-3558-49d6-8fae-209f13ccaf25&geocode=55.8581,38.4404
+// https://api.geotree.ru/address.php?key=7mAEh31NHvpF&lat=55.85810611088341&lon=38.440435433879074&types=place

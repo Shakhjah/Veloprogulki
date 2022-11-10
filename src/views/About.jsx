c65f0ae1bb8e1fcf702d24e38a634d05b58ed3dc
Igor.K <igor.k.nsk@gmail.com>
2022-11-10 22:50:01 +0700

    add show about map

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,10 +1,29 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function About({ dataMap, usName }) {
+module.exports = function About({ data, userName }) {
+  console.log('▶ ⇛ data', data);
   return (
-    <Layout userName={usName}>
+    <Layout>
+      <script
+        defer
+        src="https://api-maps.yandex.ru/2.1/?apikey=ee11c971-3558-49d6-8fae-209f13ccaf25&lang=ru_RU"
+        type="text/javascript"
+      />
       <div>In ABOUT Views</div>
+      <input type="text" id="from" name="from" value={data.mapFrom} hidden />
+      <input type="text" id="to" name="to" value={data.mapTo} hidden />
+      <div className="container">
+        <div className="row map-wrap">
+          <div className="map-block">
+            <div id="mapAbout" />
+          </div>
+          <div className="map-block">
+            <div id="mapAbout" />
+          </div>
+        </div>
+      </div>
+      <script defer src="js/mapAbout.js" />
     </Layout>
   );
 };
